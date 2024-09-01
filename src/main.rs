a116d055a12c20c4cf1ea3e80537e7b77dee2e14
misha-mad <zakharov3010@yandex.ru>
2024-09-01 20:12:38 +0200

    add delay_before_start and duration settings

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,22 @@ struct AutoClicker {
     click_interval_slider_value: u8,
     click_thread: Option<thread::JoinHandle<()>>,
     clicks_count_slider_value: u8,
+    delay_before_start_value: u8,
+    duration_value: u8,
     elapsed_time: u64,
-    is_running: bool,
+    is_running: Arc<Mutex<bool>>,
     selected_mouse_button: Arc<Mutex<MouseButton>>,
     stop_sender: Option<mpsc::Sender<()>>,
     theme: Theme,
-    total_clicks: Arc<Mutex<i32>>,
+    ticks_count: u64,
+    total_clicks: Arc<Mutex<u32>>,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     ClickCountSliderChanged(u8),
+    DelayBeforeStartSliderChanged(u8),
+    DurationSliderChanged(u8),
     IntervalSliderChanged(u8),
     SelectMouseButton(MouseButton),
     Start,
@@ -57,25 +62,33 @@ impl Application for AutoClicker {
             }
             Message::Start => {
                 println!("Starting the auto clicker");
-                self.is_running = true;
+                *self.is_running.lock().unwrap() = true;
                 self.elapsed_time = 0;
                 self.total_clicks = Arc::new(Mutex::new(0));
                 println!("click_interval_secs {}", self.click_interval_slider_value);
                 let interval = self.click_interval_slider_value;
                 let clicks_count = self.clicks_count_slider_value;
+                let duration = self.duration_value;
                 let (tx, rx) = mpsc::channel();
                 let total_clicks = Arc::clone(&self.total_clicks);
                 let selected_mouse_button = Arc::clone(&self.selected_mouse_button);
+                let is_running = Arc::clone(&self.is_running);
+                let delay_before_start = self.delay_before_start_value;
+                let end_time = std::time::Instant::now()
+                    + Duration::from_secs(duration as u64)
+                    + Duration::from_secs(delay_before_start as u64);
                 self.stop_sender = Some(tx);
 
                 let handle = thread::spawn(move || {
                     println!("interval {}", interval);
+                    thread::park_timeout(Duration::from_secs(delay_before_start as u64));
                     let mut enigo = Enigo::new(&EnigoSettings::default()).unwrap();
                     let button = selected_mouse_button.lock().unwrap().clone();
 
                     loop {
-                        if rx.try_recv().is_ok() {
-                            println!("Stop signal received");
+                        if rx.try_recv().is_ok() || std::time::Instant::now() >= end_time {
+                            *is_running.lock().unwrap() = false;
+                            println!("Stop signal received or duration elapsed");
                             break;
                         }
 
@@ -106,12 +119,15 @@ impl Application for AutoClicker {
                     }
                 }
 
-                self.is_running = false;
                 Command::none()
             }
             Message::Tick => {
-                if self.is_running {
-                    self.elapsed_time += 1;
+                if *self.is_running.lock().unwrap() {
+                    self.ticks_count += 1;
+
+                    if self.ticks_count > self.delay_before_start_value as u64 {
+                        self.elapsed_time += 1;
+                    }
                 }
 
                 Command::none()
@@ -126,12 +142,34 @@ impl Application for AutoClicker {
                 self.clicks_count_slider_value = new_clicks_count;
                 Command::none()
             }
+            Message::DelayBeforeStartSliderChanged(new_delay) => {
+                println!("Setting delay_before_start_value to {} seconds", new_delay);
+                self.delay_before_start_value = new_delay;
+                Command::none()
+            }
+            Message::DurationSliderChanged(new_duration) => {
+                println!("Setting duration_value to {} seconds", new_duration);
+                self.duration_value = new_duration;
+                Command::none()
+            }
         }
     }
 
     fn view(&self) -> Element<Self::Message> {
         let theme_text = text("Theme:");
         let pick_list = pick_list(Theme::ALL, Some(&self.theme), Message::ThemeChanged);
+
+        let delay_before_start_text = text(format!(
+            "Delay before start: {}s",
+            self.delay_before_start_value
+        ));
+
+        let delay_before_start_slider = slider(
+            0..=100,
+            self.delay_before_start_value,
+            Message::DelayBeforeStartSliderChanged,
+        );
+
         let interval_text = text(format!("Interval: {}s", self.click_interval_slider_value));
 
         let interval_slider = slider(
@@ -148,6 +186,8 @@ impl Application for AutoClicker {
             Message::ClickCountSliderChanged,
         );
 
+        let duration_text = text(format!("Duration: {}s", self.duration_value));
+        let duration_slider = slider(1..=100, self.duration_value, Message::DurationSliderChanged);
         let choose_mouse_button_text = text("Choose mouse button:");
 
         let left_button = button(text("Left"))
@@ -195,6 +235,14 @@ impl Application for AutoClicker {
             .align_items(Center)
             .spacing(10),
             horizontal_rule(20),
+            // The `Delay Before Start` section
+            row![
+                delay_before_start_text.width(FillPortion(1)),
+                delay_before_start_slider.width(FillPortion(2))
+            ]
+            .align_items(Center)
+            .spacing(10),
+            horizontal_rule(20),
             // The `Interval` section
             row![
                 interval_text.width(FillPortion(1)),
@@ -207,6 +255,14 @@ impl Application for AutoClicker {
             row![
                 clicks_count_text.width(FillPortion(1)),
                 clicks_count_slider.width(FillPortion(2))
+            ]
+            .align_items(Center)
+            .spacing(10),
+            horizontal_rule(20),
+            // The `Duration` section
+            row![
+                duration_text.width(FillPortion(1)),
+                duration_slider.width(FillPortion(2))
             ]
             .align_items(Center)
             .spacing(10),
@@ -231,22 +287,24 @@ impl Application for AutoClicker {
             horizontal_rule(20),
             row![timer_text, total_clicks_text]
                 .align_items(Center)
-                .spacing(10),
+                .spacing(10)
+                .height(FillPortion(1)),
             horizontal_rule(20),
             row![
-                start_button.on_press_maybe(if self.is_running {
+                start_button.on_press_maybe(if *self.is_running.lock().unwrap() {
                     None
                 } else {
                     Some(Message::Start)
                 }),
-                stop_button.on_press_maybe(if self.is_running {
+                stop_button.on_press_maybe(if *self.is_running.lock().unwrap() {
                     Some(Message::Stop)
                 } else {
                     None
                 }),
             ]
             .align_items(Center)
-            .spacing(10),
+            .spacing(10)
+            .height(FillPortion(1)),
         ]
         .spacing(10)
         .padding(20)
@@ -268,7 +326,7 @@ impl Application for AutoClicker {
     }
 
     fn subscription(&self) -> Subscription<Self::Message> {
-        if self.is_running {
+        if *self.is_running.lock().unwrap() {
             iced::time::every(Duration::from_millis(1000)).map(|_| Message::Tick)
         } else {
             Subscription::none()
@@ -282,11 +340,14 @@ impl Default for AutoClicker {
             click_interval_slider_value: 1,
             click_thread: None,
             clicks_count_slider_value: 1,
+            delay_before_start_value: 0,
+            duration_value: 10,
             elapsed_time: 0,
-            is_running: false,
+            is_running: Arc::new(Mutex::new(false)),
             selected_mouse_button: Arc::new(Mutex::new(MouseButton::Left)),
             stop_sender: None,
             theme: Theme::Light,
+            ticks_count: 0,
             total_clicks: Arc::new(Mutex::new(0)),
         }
     }
